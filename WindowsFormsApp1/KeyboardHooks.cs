2bea82e18bb96997d80879539a21ad1c390977a7
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-05-31 06:44:48 +0100

    base

---
+++ WindowsFormsApp1/KeyboardHooks.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class KeyboardHook : IDisposable
+    {
+        Func<Keys, bool, bool, bool, int, int> HandleKeyDown;
+
+        public delegate int CallbackDelegate(int Code, int W, int L);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct KBDLLHookStruct
+        {
+            public Int32 vkCode;
+            public Int32 scanCode;
+            public Int32 flags;
+            public Int32 time;
+            public Int32 dwExtraInfo;
+        }
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetWindowsHookEx(HookType idHook, CallbackDelegate lpfn, int hInstance, int threadId);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, int wParam, int lParam);
+
+
+
+        public enum HookType : int
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        private int HookID = 0;
+        CallbackDelegate TheHookCB = null;
+
+        //Start hook
+        public KeyboardHook(Func<Keys, bool, bool, bool, int, int> handleKey)
+        {
+            HandleKeyDown = handleKey;
+            TheHookCB = new CallbackDelegate(KeybHookProc);
+            HookID = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, TheHookCB,
+                0, //0 for local hook. eller hwnd til user32 for global
+                0); //0 for global hook. eller thread for hooken
+        }
+
+        bool IsFinalized = false;
+        ~KeyboardHook()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+        public void Dispose()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+
+        //The listener that will trigger events
+        private int KeybHookProc(int Code, int W, int L)
+        {
+            KBDLLHookStruct LS = new KBDLLHookStruct();
+            if (Code < 0)
+            {
+                return CallNextHookEx(HookID, Code, W, L);
+            }
+            try
+            {
+                KeyEvents kEvent = (KeyEvents)W;
+                Int32 vkCode = Marshal.ReadInt32((IntPtr)L);
+                if (kEvent == KeyEvents.KeyDown || kEvent == KeyEvents.SKeyDown)
+                {
+                    if (HandleKeyDown != null)
+                    {
+                        return HandleKeyDown((Keys)vkCode, GetShiftPressed(), GetCtrlPressed(), GetAltPressed(), CallNextHookEx(HookID, Code, W, L));
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                //Ignore all errors...
+            }
+
+            return CallNextHookEx(HookID, Code, W, L);
+
+        }
+
+        public enum KeyEvents
+        {
+            KeyDown = 0x0100,
+            KeyUp = 0x0101,
+            SKeyDown = 0x0104,
+            SKeyUp = 0x0105
+        }
+
+        [DllImport("user32.dll")]
+        static public extern short GetKeyState(System.Windows.Forms.Keys nVirtKey);
+
+        public static bool GetShiftPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.ShiftKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetCtrlPressed()
+        {
+
+
+            int state = GetKeyState(System.Windows.Forms.Keys.ControlKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetAltPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.Menu);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+
+        private static bool Enabled = true;
+        [STAThread]
+        static void Main()
+        {
+            var kh = new KeyboardHook(Kh_KeyDown);
+            Application.Run();
+        }
+
+        private static int  Kh_KeyDown(Keys key, bool Shift, bool Ctrl, bool Alt, int defaultResponse)
+        {
+       
+
+            if (Enabled)
+            {
+                switch (key)
+                {
+                    // you could make this load in from a file for keymaps
+                    case Keys.A:
+                        SendKeys.Send("😂");
+                        return -1;
+                    case Keys.B:
+                        SendKeys.Send("😊");
+                        return -1;
+                    case Keys.C:
+                        SendKeys.Send("❤");
+                        return -1; // this will return a -1 which will not send
+                    case Keys.D:
+                        SendKeys.Send("😒");
+                        return -1;
+                }
+            }
+
+            if (Shift && key == Keys.M)
+            {
+                Enabled = !Enabled;
+                return -1;
+            }
+
+
+
+            return defaultResponse;         // if you look in keyboardhook.cs the default response is  CallNextHookEx(HookID, Code, W, L) which will send it to next window i.e the window your on 
+
+            // so for example if we don't handle the button press will send to visual studio code but if we handle it we return -1 so won't send the real key to visual studio code
+        }
+
+    }
+}
